476b5bdfbe70e452c84e507808205589d4cbbdd5
Jethro Moller <jethrojmoller@gmail.com>
2018-02-27 22:01:12 +0200

    Added text menu

--- database.cpp
+++ database.cpp
@@ -1,9 +1,17 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
-using namespace std;
+#include "database.h"
+// using namespace std::cout;
 
 int main(int argc, char** argv){
-  cout<<"3";
+  clear();
+std::cout<<"Welcome to Student Records!"<<std::endl;
+
+  std::cout<<"Enter a number (or q to quit) and hit the Enter key..."<<std::endl;
+  for (size_t i = 0; options[i] !=""; i++) {
+    std::cout<<options[i]<<std::endl;
+  }
+
   return 0;
 }
